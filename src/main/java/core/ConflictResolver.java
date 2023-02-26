5dbfd340040520317433c75ac768ce5961fd7192
kamil khan <kamil@panoptic-qs.com>
2023-02-26 14:08:19 +0530

    refactored

---
+++ src/main/java/core/ConflictResolver.java
@@ -0,0 +1,84 @@
+package core;
+
+import task.Task;
+
+import java.util.*;
+
+public class ConflictResolver {
+
+    public List<Task> process(List<Task> tasks) {
+
+        Map<Date, List<Task>> conflictMap = new HashMap<>();
+        GregorianCalendar begin = new GregorianCalendar();
+        GregorianCalendar end = new GregorianCalendar();
+
+        for (Task task : tasks) {
+            GregorianCalendar st = (GregorianCalendar) task.startDate.clone();
+            GregorianCalendar en = (GregorianCalendar) task.endDate.clone();
+            if (begin.getTime().after(st.getTime())) {
+                begin.setTime(st.getTime());
+            }
+            if (end.getTime().before(en.getTime())) {
+                end.setTime(en.getTime());
+            }
+            while (st.getTime().before(en.getTime())) {
+                addTaskToDateMap(st, conflictMap, task);
+                st.add(Calendar.DATE, 1);
+            }
+
+            addTaskToDateMap(st, conflictMap, task);
+        }
+        System.out.println("Begin " + begin.getTime());
+        System.out.println("End " + end.getTime());
+        Map<String, Task> map = new HashMap<String, Task>();
+        while (begin.getTime().before(end.getTime())) {
+            Date key = begin.getTime();
+
+            List<Task> list = conflictMap.get(key);
+            String mapKey = getMapKey(list);
+            if (!map.containsKey(mapKey)) {
+                GregorianCalendar g1 = new GregorianCalendar();
+                g1.setTime(key);
+                GregorianCalendar g2 = new GregorianCalendar();
+                g2.setTime(key);
+                Task t = new Task(mapKey, g1, g2);
+                map.put(mapKey, t);
+            } else {
+                Task t = map.get(mapKey);
+                if (t.startDate.getTime().after(key)) {
+                    t.startDate.setTime(key);
+                }
+                if (t.endDate.getTime().before(key)) {
+                    t.endDate.setTime(key);
+                }
+            }
+            begin.add(Calendar.DATE, 1);
+        }
+
+        List<Task> orderedList = new ArrayList<>();
+        Iterator<Task> i = map.values().iterator();
+        while (i.hasNext()) {
+            orderedList.add(i.next());
+        }
+        return orderedList;
+    }
+
+    public static void addTaskToDateMap(GregorianCalendar st, Map<Date, List<Task>> conflictMap, Task task) {
+        List<Task> l = conflictMap.get(st.getTime());
+        if (l == null) {
+            l = new ArrayList<Task>();
+            conflictMap.put(st.getTime(), l);
+        }
+        l.add(task);
+    }
+
+    private String getMapKey(List<Task> list) {
+        String s = "";
+        for (Task t : list) {
+            s = s + t.name + "-";
+        }
+        s = s.substring(0, s.length() - 1);
+        return s;
+    }
+
+}
--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -1,129 +1,18 @@
 package runner;
+import core.ConflictResolver;
 import task.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+import static utils.CalendarUtils.*;
+import static utils.FileUtils.readFromFile;
 
 public class Main {
-
-    private static int getMonth(String month){
-        if(month.equals("Jan")) return 1;
-        if(month.equals("Feb")) return 2;
-        if(month.equals("Mar")) return 3;
-        if(month.equals("Apr")) return 4;
-        if(month.equals("May")) return 5;
-        if(month.equals("Jun")) return 6;
-        if(month.equals("Jul")) return 7;
-        if(month.equals("Aug")) return 8;
-        if(month.equals("Sep")) return 9;
-        if(month.equals("Oct")) return 10;
-        if(month.equals("Nov")) return 11;
-        if(month.equals("Dec")) return 12;
-        return -1;
-    }
-    public static GregorianCalendar getCalendar(int date, int month,int year){
-        GregorianCalendar s = new GregorianCalendar();
-        s.set(Calendar.MONTH,month-1); s.set(Calendar.DATE,date);s.set(Calendar.YEAR,2000 + year);
-        s.set(Calendar.HOUR_OF_DAY,1); s.set(Calendar.MINUTE,0); s.set(Calendar.SECOND,0);
-        s.set(Calendar.MILLISECOND,0);
-        return s;
-    }
-
-    private static GregorianCalendar getDate(String s){
-        StringTokenizer tokenizer = new StringTokenizer(s,"-");
-        return getCalendar(Integer.parseInt(tokenizer.nextToken()),getMonth(tokenizer.nextToken()),
-                Integer.parseInt(tokenizer.nextToken()));
-    }
-
-    public static void addTaskToDateMap(GregorianCalendar st,Map<Date,List<Task>> conflictMap, Task task){
-        List<Task> l = conflictMap.get(st.getTime());
-        if(l == null){
-            l = new ArrayList<Task>();
-            conflictMap.put(st.getTime(),l);
-        }
-        l.add(task);
-    }
-
-    private static void readFromFile(List<Task> tasks) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(new java.io.File("input.txt")));
-            String s = "";
-            while((s = reader.readLine()) != null){
-                StringTokenizer tokenizer = new StringTokenizer(s,",");
-                String taskName = "";
-                taskName = tokenizer.nextToken();
-                String startDate = tokenizer.nextToken();
-                String endDate = tokenizer.nextToken();
-                tasks.add(new Task(taskName,getDate(startDate), getDate(endDate)));
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
     public static void main(String args[]){
-        List<Task> tasks = new ArrayList<>();
-        readFromFile(tasks);
-        Map<Date,List<Task>> conflictMap = new HashMap<>();
-        GregorianCalendar begin = new GregorianCalendar();
-        GregorianCalendar end = new GregorianCalendar();
-
-        for(Task task : tasks){
-            GregorianCalendar st = (GregorianCalendar) task.startDate.clone();
-            GregorianCalendar en = (GregorianCalendar) task.endDate.clone();
-            if(begin.getTime().after(st.getTime())){
-                begin.setTime(st.getTime());
-            }
-            if(end.getTime().before(en.getTime())){
-                end.setTime(en.getTime());
-            }
-            while(st.getTime().before(en.getTime())){
-                addTaskToDateMap(st,conflictMap,task);
-                st.add(Calendar.DATE,1);
-            }
-
-            addTaskToDateMap(st,conflictMap,task);
-        }
-        System.out.println("Begin " + begin.getTime());
-        System.out.println("End " + end.getTime());
-        Map<String,Task> map = new HashMap<String, Task>();
-        while(begin.getTime().before(end.getTime())){
-            Date key = begin.getTime();
-
-            List<Task> list = conflictMap.get(key);
-            String mapKey = getMapKey(list);
-            if(!map.containsKey(mapKey)){
-                GregorianCalendar g1 = new GregorianCalendar();
-                g1.setTime(key);
-                GregorianCalendar g2 = new GregorianCalendar();
-                g2.setTime(key);
-                Task t = new Task(mapKey, g1,g2);
-                map.put(mapKey, t);
-            }else{
-                Task t = map.get(mapKey);
-                if(t.startDate.getTime().after(key)){
-                    t.startDate.setTime(key);
-                }
-                if(t.endDate.getTime().before(key)){
-                    t.endDate.setTime(key);
-                }
-            }
-            begin.add(Calendar.DATE,1);
-        }
-        List<Task> orderedList = new ArrayList<>();
-        Iterator<Task> i = map.values().iterator();
-        while(i.hasNext()){
-            orderedList.add(i.next());
-        }
-        orderedList.stream().sorted().forEach(System.out::println);
+        List<Task> tasks = readFromFile("input.txt");
+        new ConflictResolver().process(tasks).stream().sorted().forEach(System.out::println);
     }
 
-    private static String getMapKey(List<Task> list){
-        String s = "";
-        for(Task t : list){
-            s = s + t.name + "-";
-        }
-        s = s.substring(0,s.length()-1);
-        return s;
-    }
+
 }
