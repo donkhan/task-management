a96e1bedfa470ec79fc97fbe8b424c60a010cc87
kamil khan <kamil@panoptic-qs.com>
2023-02-26 14:27:13 +0530

    Atlas test data is added

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -13,11 +13,17 @@ import java.util.List;
 public class BasicTest {
 
     @Test
-    public void test_Basic() {
+    public void testBasicInput() {
         List<Task> l = new ConflictResolver().process(readFromFile("test_cases/input.txt"));
         assertEquals(l.size(),5);
         Collections.sort(l);
         assertEquals(l.get(0).name,"A");
         assertEquals(l.get(1).name,"A-B");
+    }
+
+    @Test
+    public void testAtlasApplication() {
+        List<Task> l = new ConflictResolver().process(readFromFile("test_cases/atlas.txt"));
+        assertEquals(7, l.size());
     }
 }
