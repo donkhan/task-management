ae165f87ad07b1311dd6b4ea6c56237d1957a9b4
kamil khan <kamil@panoptic-qs.com>
2023-02-26 12:03:49 +0530

    date format is changed

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -7,9 +7,24 @@ import java.util.*;
 
 public class Main {
 
+    private static int getMonth(String month){
+        if(month.equals("Jan")) return 1;
+        if(month.equals("Feb")) return 2;
+        if(month.equals("Mar")) return 3;
+        if(month.equals("Apr")) return 4;
+        if(month.equals("May")) return 5;
+        if(month.equals("Jun")) return 6;
+        if(month.equals("Jul")) return 7;
+        if(month.equals("Aug")) return 8;
+        if(month.equals("Sep")) return 9;
+        if(month.equals("Oct")) return 10;
+        if(month.equals("Nov")) return 11;
+        if(month.equals("Dec")) return 12;
+        return -1;
+    }
     public static GregorianCalendar getCalendar(int date, int month,int year){
         GregorianCalendar s = new GregorianCalendar();
-        s.set(Calendar.MONTH,month-1); s.set(Calendar.DATE,date);s.set(Calendar.YEAR,year);
+        s.set(Calendar.MONTH,month-1); s.set(Calendar.DATE,date);s.set(Calendar.YEAR,2000 + year);
         s.set(Calendar.HOUR_OF_DAY,1); s.set(Calendar.MINUTE,0); s.set(Calendar.SECOND,0);
         s.set(Calendar.MILLISECOND,0);
         return s;
@@ -17,7 +32,8 @@ public class Main {
 
     private static GregorianCalendar getDate(String s){
         StringTokenizer tokenizer = new StringTokenizer(s,"-");
-        return getCalendar(Integer.parseInt(tokenizer.nextToken()),Integer.parseInt(tokenizer.nextToken()),Integer.parseInt(tokenizer.nextToken()));
+        return getCalendar(Integer.parseInt(tokenizer.nextToken()),getMonth(tokenizer.nextToken()),
+                Integer.parseInt(tokenizer.nextToken()));
     }
 
     public static void addTaskToDateMap(GregorianCalendar st,Map<Date,List<Task>> conflictMap, Task task){
